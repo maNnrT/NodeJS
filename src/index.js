7e69c3bbe4869fc11e203979dab56fa5d7e4390b
trannam <namphongtran232@gmail.com>
2023-09-08 17:49:43 +0700

    Install Prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 
 
-            
-app.listen(port, () => {
+
+          app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
