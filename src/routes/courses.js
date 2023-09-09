b069950f4d18bbdd7bddc6da63c269827edc1a41
trannam <namphongtran232@gmail.com>
2023-09-10 00:40:29 +0700

    "Select all" with checkbox

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
+router.post('/handle-form-actions', courseController.handleFormActions);
 router.post('/store', courseController.store);
 router.get('/:slug', courseController.show);
 router.get('/:id/edit', courseController.edit);
