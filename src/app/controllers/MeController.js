f2fe7ab6348c10b2355a26bb40491a709b2f47e7
trannam <namphongtran232@gmail.com>
2023-09-09 23:40:22 +0700

    Count delete courses

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,11 +5,13 @@ const {
 class MeController {
 	// GET /me/stored/courses
 	storedCourses(req, res,next) {
-		Course.find({})
-			.then((courses) => res.render('me/stored-courses', {
-				courses: multipleMongooseToObject(courses)
-			}))
-		.catch(next)
+		Promise.all([Course.find({}), Course.countDocumentsWithDeleted({deleted:true})])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
 	}
 	// [GET] /me/trash/courses
     trashCourses(req, res, next) {
